327c0411341bee7e01211ff233ef672c65cb37eb
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2021-04-26 20:00:03 +0100

    Update initialise.c

--- OthelloAssignment_20338321/initialise.c
+++ OthelloAssignment_20338321/initialise.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include "initialise.h"
+// importing the required libraries
 
 void initializePlayers(player InfoPlayer[])
-{
-    int choice, checker = 1;
+{ // Function to initialise the players names, colour
+
+    int choice, checker = 1; // declaring variables
 
     printf("\n\t\t\t***  Welcome to Othello!  ***\n\n");
 
+    // initialise the player struct with their name
     printf("\nPlayer 1 please enter your name:\n");
     scanf("%s", InfoPlayer[0].name);
     printf("\nPlayer 2 please enter your name:\n");
     scanf("%s", InfoPlayer[1].name);
 
+    // initialise the player struct with their colours
     while(checker == 1) {
         printf("\n\n%s(player 1) please chose a colour:\n\n"
                "For white type: 1,\n"
@@ -43,7 +47,8 @@ void initializePlayers(player InfoPlayer[])
 }
 
 void startBoard(board infoBoard[], player InfoPlayer[])
-{
+{ // Function to initialise the beginning of the struct
+    // initialise the 4 middle squares and saving it in the board struct
     for(int i=0;i<8;i++){
         for(int j=0;j<8;j++){
             if((i == 3 && j == 3) || (i == 4 && j == 4)){
@@ -57,18 +62,22 @@ void startBoard(board infoBoard[], player InfoPlayer[])
             }
         }
     }
+    // initialise the beginning struct
     InfoPlayer[0].score = 2;
     InfoPlayer[1].score = 2;
 }
 
 void boardPrinter(board info[], player InfoPlayer[])
-{
+{ // Function to display the board
+
+    // if statement to check which player is black and display their score and name at the left side
     if(strcmp(InfoPlayer[0].colour, "(B)") == 0){
         printf("\n\n\tScore: %s(Black %s) %d:%d %s(White %s)\n", InfoPlayer[0].name, InfoPlayer[0].colour, InfoPlayer[0].score, InfoPlayer[1].score,InfoPlayer[1].name, InfoPlayer[1].colour);
     }
     else{
         printf("\n\tScore: %s (Black %s) %d:%d %s (White %s)\n", InfoPlayer[1].name, InfoPlayer[1].colour, InfoPlayer[1].score, InfoPlayer[0].score,InfoPlayer[0].name, InfoPlayer[0].colour);
     }
+    // displays the board to the user
     printf("\n     a     b     c     d     e     f     g     h  ");
     puts("\n   _____ _____ _____ _____ _____ _____ _____ _____");
     for(int i=0;i<8;i++){
@@ -82,7 +91,7 @@ void boardPrinter(board info[], player InfoPlayer[])
 }
 
 void initializeBeg(player InfoPlayer[], board infoBoard[])
-{
+{ // function to call the three functions
     initializePlayers(InfoPlayer);
     startBoard(infoBoard, InfoPlayer);
     boardPrinter(infoBoard, InfoPlayer);
