d8bd74557dc597b9bf412a1c27f42cfccfbf1821
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2021-04-26 20:42:32 +0100

    Update othello.c

--- OthelloAssignment_20338321/othello.c
+++ OthelloAssignment_20338321/othello.c
@@ -3,9 +3,10 @@
 #include <stdbool.h>
 #include <time.h>
 #include "ValidMove.h"
+// importing the required libraries
 
 int playerTurn(int currentTurn)
-{
+{ // function to check the turn and return the other player turn either 0 or 1
     if(currentTurn == 0) {
         return 1;
     }
@@ -15,7 +16,7 @@ int playerTurn(int currentTurn)
 }
 
 int column(const char col)
-{
+{ // Function that convert the letter to number for the column index
     int val=-1;
     switch (col){
         case 'a':
@@ -49,7 +50,7 @@ int column(const char col)
 }
 
 void playerScores(board infoBoard[], player InfoPlayer[])
-{
+{ // updating the score for the player
     int player1Score = 0, player2Score = 0;
     for(int i=0;i<8;i++){
         for(int j=0;j<8;j++){
@@ -66,7 +67,7 @@ void playerScores(board infoBoard[], player InfoPlayer[])
 }
 
 void finalBoardAppendToFile(board info[], player InfoPlayer[], FILE *fptr)
-{
+{ // appends the final board to the file
     if(strcmp(InfoPlayer[0].colour, "(B)") == 0){
         fprintf(fptr,"\n\n\tScore: %s(Black %s) %d:%d %s(White %s)\n", InfoPlayer[0].name, InfoPlayer[0].colour, InfoPlayer[0].score, InfoPlayer[1].score,InfoPlayer[1].name, InfoPlayer[1].colour);
     }
@@ -86,7 +87,7 @@ void finalBoardAppendToFile(board info[], player InfoPlayer[], FILE *fptr)
 }
 
 void winnerResultDisplayPrinter(board infoBoard[], player InfoPlayer[], time_t timetaken)
-{
+{ // Function that displays the winner/draw to the user and board and time of the game and date and append to the file with all that info othello-results.txt
     struct tm* ptr;
     time_t lt;
     lt = time(NULL);
@@ -153,27 +154,32 @@ void winnerResultDisplayPrinter(board infoBoard[], player InfoPlayer[], time_t t
 
 
 void playGame(player InfoPlayer[], board infoBoard[])
-{
+{ // function that plays the game
+    // declaring variables
     char col, colm[0];
     int playerCurrent, row=0;
     bool checker = false;
     time_t start, end;
     start = time(NULL);
+
+    // sets the black player to play first
     if(strcmp(InfoPlayer[0].colour, "(B)") == 0){
         playerCurrent = 0;
     }
     else{
         playerCurrent = 1;
     }
+    // loops while their is a available move
     while(availablePlayerMoves(InfoPlayer, infoBoard, 0) || availablePlayerMoves(InfoPlayer, infoBoard, 1)){
-        if(availablePlayerMoves(InfoPlayer, infoBoard, 0) == false){
+        if(availablePlayerMoves(InfoPlayer, infoBoard, 0) == false){ // if their is no available move for player 1 it passes to the next player
             printf("\nNo available moves for %s(player 1), Passed to the next player\n", InfoPlayer[0].name);
             playerCurrent = 1;
         }
-        if(availablePlayerMoves(InfoPlayer, infoBoard, 1) == false){
+        if(availablePlayerMoves(InfoPlayer, infoBoard, 1) == false){ // if their is no available move for player 1 it passes to the next player
             printf("\nNo available moves for %s(player 2), Passed to the next player\n", InfoPlayer[1].name);
             playerCurrent = 0;
         }
+        // getting the input from the user for the postion they want to place their disc and keeps loops unless it is valid move
         printf("\n%s(player %d) Please enter the rows and columns you want to position(i.e d2): \n", InfoPlayer[playerCurrent].name, playerCurrent+1);
         scanf("%1s%d", colm, &row);
         col = colm[0];
@@ -190,10 +196,12 @@ void playGame(player InfoPlayer[], board infoBoard[])
                 checker = makeMove(InfoPlayer, infoBoard, playerCurrent);
             }
         }
+        // displays the board and players score and set the next player
         playerScores(infoBoard, InfoPlayer);
         boardPrinter(infoBoard, InfoPlayer);
         playerCurrent = playerTurn(playerCurrent);
     }
+    // if their is no more valid move for either player it calls the function that diplays the winner,time,date,score, and append all that info to the file
     if((availablePlayerMoves(InfoPlayer, infoBoard, 0) == false) && (availablePlayerMoves(InfoPlayer, infoBoard, 1) == false)){
         end = time(NULL);
         winnerResultDisplayPrinter(infoBoard, InfoPlayer, difftime(end, start));
