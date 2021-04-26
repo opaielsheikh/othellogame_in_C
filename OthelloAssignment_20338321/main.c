3b113ea36bfed858abcde8b7753f0c97ea514d12
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2021-04-26 20:44:53 +0100

    Update main.c

--- OthelloAssignment_20338321/main.c
+++ OthelloAssignment_20338321/main.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include "othello.h"
+// importing the required libraries 
 
 int main(void)
 {
+    // declaring variables 
     struct tm* ptr;
     time_t lt;
     lt = time(NULL);
     ptr = localtime(&lt);
-    printf("\n\t\t\t testing: %s", asctime(ptr));
+    
+    // display the time and calling the function for the othello game
+    printf("\n\t\t\t %s", asctime(ptr));
     player *Players = malloc(sizeof(char));
     board *Board = malloc(sizeof(char));
     initializeBeg(Players, Board);
     playGame(Players, Board);
     return 0;
 }
-
