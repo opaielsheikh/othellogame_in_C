5efdd23fb11dc17a71d180613c8a17be84c5e6f6
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2021-04-26 19:52:05 +0100

    Update initialise.h

--- OthelloAssignment_20338321/initialise.h
+++ OthelloAssignment_20338321/initialise.h
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+// importing the required files
 
+// struct
 typedef struct{
     char name[50];
     char colour[50];
     int score;
 }player;
 
+// struct
 typedef struct{
     char boards[20][10];
     int rows;
@@ -21,6 +24,7 @@ typedef struct{
     int diagonalSide;
 }board;
 
+// functions prototypes
 void initializePlayers(player InfoPlayer[]);
 void startBoard(board infoBoard[], player InfoPlayer[]);
 void boardPrinter(board info[], player InfoPlayer[]);
